3b24b300deaaaeff073edd7c6051e3d2fac9743a
alexfisher13 <verjil13>
2019-07-15 14:55:14 +0300

    add credit calculation

--- src/main/java/trello/MainController.java
+++ src/main/java/trello/MainController.java
@@ -6,13 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller //Все http запросы обрабатываются контроллером
-public class MainController {
+import java.text.NumberFormat;
+import java.util.Locale;
 
-    // Аннотация @RequestMapping гарантирует, что HTTP запросы к /greeting приведут к выполнению метода greeting()
-    // @RequestParam связывает значение параметра name запроса, с name параметром метода greeting().
-    // Этот параметр не required; если он отсутствует в запросе, то будет использовано defaultValue значение "World".
-    // Значение параметра name добавлено в объект Model, что делает его доступным в шаблоне представления.
+@Controller
+public class MainController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
@@ -26,22 +24,47 @@ public class MainController {
     }
 
     @PostMapping("/")
-    public String calc(@RequestParam(required = false, defaultValue = "10") int time,
-                       @RequestParam(required = false, defaultValue = "5000000") int price,
-                       @RequestParam(required = false, defaultValue = "1000000") int first,
-                       @RequestParam(required = false, defaultValue = "10") int rate,
-                       @RequestParam(required = false, defaultValue = "20000") int rent,
+    public String calc(@RequestParam(required = false, defaultValue = "10") int years,
+                       @RequestParam(required = false, defaultValue = "4000000") int loan,
+                       @RequestParam(required = false, defaultValue = "10") float rate,
+                       @RequestParam(required = false, defaultValue = "24000") int rent,
+                       @RequestParam(required = false, defaultValue = "6") float percent,
                        Model model) {
 
-        Long pay = null;
-        Long total = null;
-        Long overpay = null;
+        int pay = payCalculate(loan, years, rate);
+        int total = pay * years * 12;
+        int overpay = total - loan;
 
+        int diff = pay - rent;
+        float mPersent = percent/100/12;
+        int msave = saveCalculate(diff, 12, mPersent);
+        int ysave = saveCalculate(diff, years * 12, mPersent);
 
-        model.addAttribute("pay", pay);
-        model.addAttribute("total", total);
-        model.addAttribute("overpay", overpay);
+        Locale loc = new Locale("ru");
+        NumberFormat formatter = NumberFormat.getInstance(loc);
+
+        model.addAttribute("pay", formatter.format(pay));
+        model.addAttribute("total", formatter.format(total));
+        model.addAttribute("overpay", formatter.format(overpay));
+        model.addAttribute("diff", formatter.format(diff));
+        model.addAttribute("msave", formatter.format(msave));
+        model.addAttribute("ysave", formatter.format(ysave));
         return "main";
     }
 
+    private int payCalculate(long loan, int years, float rate) {
+        int months = years * 12;
+        double mRate = rate/100/12;
+        double a = mRate * Math.pow(1 + mRate, months);
+        double b = Math.pow(1 + mRate, months) - 1;
+        return (int) (loan * a/b);
+    }
+
+    private int saveCalculate(int diff, int months, float percent) {
+        float save = diff;
+        for (int i = 0; i < months - 1; i++) {
+            save = save + diff + save * percent;
+        }
+        return (int) save;
+    }
 }
